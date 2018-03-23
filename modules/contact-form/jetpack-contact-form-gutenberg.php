e9264fdda7dccbdda152e8d3ab04026287277d8f
Louis Laugesen <louis.laugesen@gmail.com>
2018-03-24 02:28:42 +1100

    Remove Contact Form Gutenberg class
    Class is just a shell for the minute, may as well delete!
    
    Moved hooks into module jetpack-contact-form-gutenberg.php

--- modules/contact-form/jetpack-contact-form-gutenberg.php
+++ modules/contact-form/jetpack-contact-form-gutenberg.php
@@ -1,13 +1,32 @@
 <?php
 /**
- * Jetpack Contact Form - Gutenberg
+ * Jetpack Gutenberg Form
  *
- * This file loads Gutenberg block support for the Jetpack contact form.
+ * This file loads Gutenberg block support for the Jetpack contact form module.
  *
  * @package Jetpack\modules\contact-form
  * @since 5.9
  */
-require_once dirname( __FILE__ ) . '/class-jetpack-contact-form-gutenberg.php';
 
-add_action( 'init', array( 'Jetpack_Contact_Form_Gutenberg', 'init' ) );
-add_action( 'enqueue_block_editor_assets', array( 'Jetpack_Contact_Form_Gutenberg', 'enqueue_block_editor_assets' ) );
+function jetpack_form_init() {
+	register_block_type(
+		'jetpack/form',
+		array(
+			'render_callback' => function ( $args ) {
+				return '<pre>' . print_r( $args, true ) . '</pre>';
+			},
+		)
+	);
+}
+
+function jetpack_form_enqueue_editor() {
+	wp_enqueue_script(
+		'jetpack-contact-form-gutenberg',
+		plugins_url( 'js/jetpack-contact-form-gutenberg.js', __FILE__ ),
+		array( 'wp-blocks', 'wp-i18n', 'wp-element' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'js/jetpack-contact-form-gutenberg.js' )
+	);
+}
+
+add_action( 'init', 'jetpack_form_init' );
+add_action( 'enqueue_block_editor_assets', 'jetpack_form_enqueue_editor' );
