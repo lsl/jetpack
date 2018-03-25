022f3f7b266935061cf559f1c5cb453a8a1be6a1
Louis Laugesen <louis.laugesen@gmail.com>
2018-03-25 19:57:05 +1100

    Separate block definition to reduce indentation

--- modules/contact-form/js/jetpack-contact-form-gutenberg.js
+++ modules/contact-form/js/jetpack-contact-form-gutenberg.js
@@ -9,6 +9,7 @@
 	var InnerBlocks = blocks.InnerBlocks;
 	var TextControl = components.TextControl;
 
+	// Return a setter for given props and key
 	function setAttributes( props, key ) {
 		return function ( value ) {
 			attrs = {};
@@ -18,78 +19,78 @@
 		}
 	}
 
-	// Register the form block under jetpack/form
-	blocks.registerBlockType(
-		'jetpack/form',
-		{
-			// Standard attributes
-			title : i18n.__( 'Form' ),
-			icon : 'feedback',
-			category : 'common',
+	// Define the form block
+	var Form = {
+		title : i18n.__( 'Form' ),
+		icon : 'feedback',
+		category : 'common',
+		attributes: {
+			subject : {
+				type : 'string',
+				default : '',
 
-			// Defaults
-			attributes : {
-				subject : {
-					type : 'string',
-					default : '',
-				},
-				to : {
-					type: 'string',
-					default: ''
-				},
 			},
-
-			// Display the form in the editor
-			edit : function( props ) {
-				return [
-					// Display the inner blocks
-					el(
-						InnerBlocks, {
-							key: 'jetpack/form/innerblocks',
-							layouts: {
-								stacked: { label: 'Stacked', icon: 'align-left' },
-								horizontal: { label: 'Horizontal', icon: 'align-wide' },
-							}
-						}
-					),
-					// Display Inspector Controls
-					! ! props.focus && el(
-						InspectorControls,
-						{ key : 'inspector' },
-						[
-							el(
-								TextControl,
-								{
-									key : 'jetpack/form/inspector/subject',
-									onChange : setAttributes( props, 'subject' ),
-									value : props.attributes.subject,
-									placeholder: i18n.__( '[Site Feedback]' ),
-									label : i18n.__( 'What would you like the subject line of the email to be?' )
-								}
-							),
-							el(
-								TextControl,
-								{
-									key : 'jetpack/form/inspector/to',
-									onChange : setAttributes( props, 'to' ),
-									value : props.attributes.to,
-									placeholder: i18n.__( 'admin@example.com' ),
-									label : i18n.__( 'Which email address should we send the submissions to?' )
-								}
-							)
-						]
-					),
-				];
-			},
-
-			save : function( props ) {
-				return el(
-					'div', { className: props.className },
-					el( InnerBlocks.Content )
-				);
+			to : {
+				type: 'string',
+				default: ''
 			}
 		}
-	);
+	};
+
+	// Render the editor
+	Form.edit = function edit( props ) {
+		return [
+			// Display the inner blocks
+			el(
+				InnerBlocks, {
+					key: 'jetpack/form/innerblocks',
+					layouts: {
+						stacked: { label: 'Stacked', icon: 'align-left' },
+						horizontal: { label: 'Horizontal', icon: 'align-wide' },
+					}
+				}
+			),
+			// Display on Focus
+			! ! props.focus &&
+			el(
+				InspectorControls,
+				{ key : 'inspector' },
+				[
+					el(
+						TextControl,
+						{
+							key : 'jetpack/form/inspector/subject',
+							onChange : setAttributes( props, 'subject' ),
+							value : props.attributes.subject,
+							placeholder: i18n.__( '[Site Feedback]' ),
+							label : i18n.__( 'What would you like the subject line of the email to be?' )
+						}
+					),
+					el(
+						TextControl,
+						{
+							key : 'jetpack/form/inspector/to',
+							onChange : setAttributes( props, 'to' ),
+							value : props.attributes.to,
+							placeholder: i18n.__( 'admin@example.com' ),
+							label : i18n.__( 'Which email address should we send the submissions to?' )
+						}
+					)
+				]
+			),
+		];
+	};
+
+	// Render the form content
+	Form.save = function save( props ) {
+		return el(
+			'div', { className: props.className },
+			el( InnerBlocks.Content )
+		);
+	}
+
+	// Register the form block under jetpack/form
+	blocks.registerBlockType( 'jetpack/form', Form );
 } )(
 	window.wp.blocks,
 	window.wp.components,
