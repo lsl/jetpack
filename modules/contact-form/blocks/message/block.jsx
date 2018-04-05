bec25e0abe90a93c46643cb9e125c71f14b7e79c
Louis Laugesen <louis.laugesen@gmail.com>
2018-04-05 22:05:57 +1000

    Replace spaces with tabs

--- modules/contact-form/blocks/message/block.jsx
+++ modules/contact-form/blocks/message/block.jsx
@@ -94,11 +94,11 @@ let Form = {
 				<ToggleControl
 					key="jetpack/form-text/required"
 					label={ __( 'Is required?' ) }
-			        help={ __( 'Should the user be forced to enter text into this field?' ) }
-			        checked={ attributes.required  }
-			        onChange={ value => setAttributes( { 'required': value } ) }
-			        // todo: onChange probably broken -
-			        // From gutenberg:paragraph onChange={ this.toggleDropCap }
+					help={ __( 'Should the user be forced to enter text into this field?' ) }
+					checked={ attributes.required  }
+					onChange={ value => setAttributes( { 'required': value } ) }
+					// todo: onChange probably broken -
+					// From gutenberg:paragraph onChange={ this.toggleDropCap }
 				/>
 				<TextControl
 					key="jetpack/form-text/rows"
